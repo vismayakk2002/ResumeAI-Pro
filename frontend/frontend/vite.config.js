a1fdde45df3552d2f4564bd8dce1742100cb6ae3
VISMAYA K K <vismayapradeep294@gmail.com>
2026-07-20 00:29:19 +0530

    allow railway frontend host

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      "superb-trust-production-b12d.up.railway.app"
+    ]
+  }
 })
