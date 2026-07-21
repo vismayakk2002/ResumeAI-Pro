41a9542464d5d21a4eac5a329decfdd583cf130d
VISMAYA K K <vismayapradeep294@gmail.com>
2026-07-21 21:58:19 +0530

    Commit

--- frontend/frontend/src/api.js
+++ frontend/frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
+  // baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
+  baseURL: "https://resumeai-pro-production-6b0e.up.railway.app",
+
 });
 
 export function setAuthToken(token) {
